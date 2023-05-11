748107f549ca0cf77e1553525c0cc3945bbdbc1c
Andrew Senichkin <Kniws10@yandex.ru>
2023-05-11 18:59:09 +0300

    fix bugs _id

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const routes = require('./routes/router');
-// mongodb://localhost:27017 через этот порт не подключался, подключил через терминал с помощью mongosh
+
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
 const { PORT = 3000 } = process.env;
 const app = express();
+
 app.use(helmet());
+app.disable('x-powered-by');
 app.use(express.json());
-app.use(routes);
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '645c9465c38af38ed961aea1', // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '645c9465c38af38ed961aea1', // ID пользователя из mongo.
   };
 
   next();
 });
 
-mongoose.connect(URL)
+app.use(routes);
+
+// Данный адрес взят после подключения через терминал с помощью mongosh:
+mongoose
+  .connect(URL)
   .then(() => {
     console.log('БД подключена');
   })
