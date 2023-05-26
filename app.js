87bed4d3145406b8ee90272798880530e4b980dd
Andrew Senichkin <Kniws10@yandex.ru>
2023-05-26 12:06:00 +0300

    fix

--- app.js
+++ app.js
@@ -6,23 +6,17 @@ const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const routes = require('./routes/router');
-const { login, createUser } = require('./controllers/users');
 require('dotenv').config();
 const NotFoundError = require('./errors/NotFoundError');
+const signup = require('./routes/signup');
+const signin = require('./routes/signin');
 
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
 const { PORT = 3000 } = process.env;
-const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
   max: 100, // можно совершить максимум 100 запросов с одного IP
 });
-
-// подключаем rate-limiter
-app.use(limiter);
-app.use(helmet());
-app.disable('x-powered-by');
-app.use(express.json());
 
 // Данный адрес взят после подключения через терминал с помощью mongosh:
 mongoose
@@ -34,15 +28,18 @@ mongoose
     console.log('Не удалось подключиться к БД');
   });
 
+const app = express();
+// подключаем rate-limiter
+app.use(limiter);
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-app.post('/signin', login);
-app.post('/signup', createUser);
-
+app.use('/', signup);
+app.use('/', signin);
 app.use(routes);
-
 app.use(errors());
 
 app.use((req, res, next) => next(new NotFoundError('Страницы по запрошенному URL не существует')));
