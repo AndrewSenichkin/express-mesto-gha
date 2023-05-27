6821b1e55b8129df2342f1c348e6ddbc74626988
Andrew Senichkin <Kniws10@yandex.ru>
2023-05-27 11:42:10 +0300

    fix

--- routes/router.js
+++ routes/router.js
@@ -3,13 +3,7 @@ const auth = require('../middlewares/auth');
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
-const NotFoundError = require('../errors/NotFoundError');
-
 router
   .use('/users', auth, userRoutes)
-  .use('/cards', auth, cardRoutes)
-  .use('/*', auth, (req, res, next) => {
-    next(new NotFoundError('Запрашиваемый ресурс не найден'));
-  });
-
+  .use('/cards', auth, cardRoutes);
 module.exports = router;
